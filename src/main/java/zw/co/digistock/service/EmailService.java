c3c8349a49d0c97844fcf884b4a05f567d032c00
Tendai Katsande <tendaikatsande@live.com>
2025-11-12 10:28:55 +0200

    refactor: Clean up EmailService by removing unused imports

--- src/main/java/zw/co/digistock/service/EmailService.java
+++ src/main/java/zw/co/digistock/service/EmailService.java
@@ -1,16 +1,11 @@
 package zw.co.digistock.service;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
 
 /**
  * Service for sending emails
